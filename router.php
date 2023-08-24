146e59447564added2c71554fbfa088427c984c9
cranzister <140072908+cranzister@users.noreply.github.com>
2023-08-24 15:07:10 +0400

    Update router.php

--- router.php
+++ router.php
@@ -4,9 +4,6 @@
     session_start();
     $mysqli = new mysqli('localhost', 'root', '', 'projectsynnn');
     require_once('classes/User.php');
-    $path = $_SERVER['REQUEST_URI'];
-    $method = $_SERVER['REQUEST_METHOD'];
     $content = "Страница не найдена";
- ?> 
-   
+ ?>    
 </html>  
